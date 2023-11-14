7fffab301b0e1d5689ffa34218c287cc496fe92d
Christinec02 <149406910+Christinec02@users.noreply.github.com>
2023-11-13 21:39:40 -0500

    Files Updated (#4)
    * Add files via upload
    
    An incomplete landing page/homepage for the project.
    Matches somewhat visually but links and input boxes are not complete.
    
    * Rename homepage.html to PARK/homepage.html
    
    * Delete magnifying-glass.png
    
    * Delete parking-garage.jpeg
    
    * Rename style.css to PARK/style.css
    
    * Add files via upload
    
    * Rearranged files into appropiate folders. Files also updated.
    
    ---------
    
    Co-authored-by: Christine Chukwunyere <christinechukwunyere@nwk-67-110.nwk.ruw.rutgers.edu>

---
+++ PARK/static/scripts/homepage.js
@@ -0,0 +1,40 @@
+// hourly and monthly buttons that change search parameters
+document.addEventListener('DOMContentLoaded', (event) => {
+    const hourlyBtn = document.getElementsByClassName('hourly')[0];
+    const monthlyBtn = document.getElementsByClassName('monthly')[0];
+  
+    hourlyBtn.addEventListener('click', function() {
+      hourlyBtn.classList.add('active');
+      monthlyBtn.classList.remove('active');
+      // Your existing code...
+    });
+  
+    monthlyBtn.addEventListener('click', function() {
+      monthlyBtn.classList.add('active');
+      hourlyBtn.classList.remove('active');
+      // Your existing code...
+    });
+  });
+
+  // hourly and monthly buttons that change search parameters
+document.addEventListener('DOMContentLoaded', (event) => {
+    const hourlyBtn = document.getElementsByClassName('hourly')[0];
+    const monthlyBtn = document.getElementsByClassName('monthly')[0];
+    const startTime = document.getElementById('start-time');
+    const endTime = document.getElementById('end-time');
+  
+    hourlyBtn.addEventListener('click', function() {
+      hourlyBtn.classList.add('active');
+      monthlyBtn.classList.remove('active');
+      startTime.style.display = 'inline-block';
+      endTime.style.display = 'inline-block';
+    });
+  
+    monthlyBtn.addEventListener('click', function() {
+      monthlyBtn.classList.add('active');
+      hourlyBtn.classList.remove('active');
+      startTime.style.display = 'none';
+      endTime.style.display = 'none';
+    });
+  });
+  
